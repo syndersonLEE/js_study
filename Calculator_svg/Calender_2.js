8d745feef3d46cfbf7ea5607818741074c394e19
Unknown <winnery93@gmail.com>
2018-07-02 17:49:03 +0900

    Calender_svg 수정 파이널

--- Calculator_svg/Calender_2.js
+++ Calculator_svg/Calender_2.js
@@ -1,14 +1,16 @@
-var boolOperNum = false; //마지막 행동 확인용
-var calculMethod = ""; //수식 변수
+var boolOperNum = false; //Operation이 마지막일경우 true 아닐경우는 false
+var calculMethod = ""; //수식에 대한 모든 string****
+var queueData = []; //그래프 수식을 위한 Queue 변수
+//display는 model만을 담당
 
-function clearNumDisplay(value){
+function clearNumDisplay(value){ // 계산기 화면을 지우기 위한 함수
   document.getElementById("display").textContent = value;
   calculMethod = value;
   boolOperNum = false;
 }
 
 function addNumDisplay(numValue){ //숫자를 더하는 경우
-  if(boolOperNum){
+  if(boolOperNum){ //첫 숫자인 경우 화면클리어
     document.getElementById("display").textContent = "";
   }
   document.getElementById("display").textContent += numValue;
@@ -17,13 +19,37 @@ function addNumDisplay(numValue){ //숫자를 더하는 경우
 }
 
 function operNum(operValue){
-  if(boolOperNum){ //수식을 바꿀경우
+  if(boolOperNum){ //수식을 바꿀경우 -> calculMethod 변수 안 맨 끝 수식 제거
       calculMethod = calculMethod.substr(0,calculMethod.length-1);
   }
   calculMethod = eval(calculMethod);
   document.getElementById("display").textContent = calculMethod;
-  if(operValue!='='){
+  if(operValue!='='){ //=이 아닐 경우에는 calculMethod에 수식 추가
       calculMethod += operValue;
       boolOperNum = true;
+  }
+  else{ //= 일 경우 수식 추가 없이 그래픽 그리기
+    addDataQueue(calculMethod);
+    displayBar();
+  }
+}
+
+//graph function
+
+function addDataQueue(displayedValue){ //큐에 마지막 변수 삽입
+  if(queueData.length < 5){
+    queueData.push(displayedValue);
+  }
+  else{
+    queueData.shift();
+    queueData.push(displayedValue);
+  }
+}
+
+function displayBar(){ //queue에서 데이터를 꺼내서 표시
+  var bars = document.getElementsByClassName("recBar");
+  for(var i = 0; i<queueData.length; i++){
+    bars[i].setAttribute("height", queueData[i]*2.916); //길이 변환(그래프 길이에 맞춰서 크기 조절)
+    bars[i].setAttribute("y", 380-(queueData[i]*2.916));//그래프 위치에 따른 조정
   }
 }
---
+++ Calculator_svg/README.md
@@ -0,0 +1,41 @@
+﻿Calculator_svg 실습
+================
+
+'svg' 를 이용해서 기존 'Calculator_2'의 html을 수정하고 그래프를 삽입합니다.
+
+svg 태그로 수정
+-----------
+``` r        
+<g class="calculPanel" width="400" font-family="Comic Sans MS">
+	<rect x="30" y="10" rx="20" ry="20" width="300" height="70" style="fill:#ccffff;" />
+	<text id="display" font-size="16" x="60" y="50"></text>
+</g>
+```
+기존 div로 구성되어 있던 태그들은 전부 svg로 수정하였음
+예시 코드는 display부분을 수정한것
+
+그래프로 변환
+-----------
+
+``` r        
+function addDataQueue(displayedValue){
+  if(queueData.length < 5){
+    queueData.push(displayedValue);
+  }
+  else{
+    queueData.shift();
+    queueData.push(displayedValue);
+  }
+}
+```
+
+
+큐를 이용해서 수식을 계산할 때마다 데이터에 그래프로 변환해 줌
+
+#### 실습간 미해결 과제 및 의문점
+
+1.text 크기 제한이 안되어 숫자가 오버 될 경우에는 뚫고 지나감 -> svg 태그 안에 svg 태그가 들어가지 않기 때문에, 이미 그려진 화면 위에 제한은 불가
+
+2.그려진 태그간의 부모 자식 관계가 g태그 제외하고 없는가?
+
+
